245903ff3799a6943eeb165409eda9d4e351ddee
Shushanna <shushanna.ghushchyan@webwork-tracker.com>
2025-11-06 13:17:24 +0400

    ...

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+
+// detect environment
+const isProduction = process.env.NODE_ENV === 'production';
+
+// set backend target based on environment
+const backendUrl = isProduction
+    ? 'https://laravel-to-doooo.vercel.app'   // 👈 your deployed backend (Vercel domain or API domain)
+    : 'http://127.0.0.1:8000';            // 👈 local Laravel backend
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -18,7 +27,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8000',
+                target: backendUrl,
                 changeOrigin: true,
                 secure: false,
             },
